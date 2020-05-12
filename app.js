54c2d7866928d6c5407c946892247fc8d40124c4
QiZhang <jason.qizhang.b@gmail.com>
2020-05-12 15:53:15 +0800

    Added pages and resource

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ var app = express()
 app.use('/public', express.static(path.join(__dirname, './public')))
 app.use('/node_modules', express.static(path.join(__dirname, './node_modules')))
 
+app.engine('html', require('express-art-template'))
+app.set('views', path.join(__dirname, './views'))
+
 app.get('/', function (req, res) {
-    res.send('Hello world')
+    res.render('index.html')
 })
 
 app.listen(3000, function () {
