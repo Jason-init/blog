2615e9464f6e2d0b1aa08150b6427639fb44e538
QiZhang <jason.qizhang.b@gmail.com>
2020-05-12 20:09:15 +0800

    Added User modul

---
+++ models/user.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Qi on 2020/5/12.
+ */
+var mongoose = require('mongoose')
+
+var Schema = mongoose.Schema
+
+var userSchema = new Schema ({
+    email: {
+        type: String,
+        required: true
+    },
+    nickname: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    created_time: {
+        type: Date,
+        default: Date.nowDate
+    },
+    last_modified_time: {
+        type: Date,
+        default: Date.now
+    },
+    avatar: {
+        type: String,
+        default: '/public/img/avatar-default.png'
+    },
+    bio: {
+        type: String,
+        default: ''
+    },
+    gender: {
+        type: Number,
+        enum: [-1, 0, 1],
+        default: -1
+    },
+    birthday: {
+        type: Date
+    },
+    status: {
+        type: Number,
+        enum: [0, 1, 2],
+        default: 0
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
+
